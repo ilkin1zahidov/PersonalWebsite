373328f2942580ccfa95fa8b156566a78766b772
ilkin1Zahidov <ilkinzahidov01@gmail.com>
2023-02-08 10:10:03 +0400

    fixed navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ function navbar() {
             <div className="container">
                 <div className="nav-area">
                     <ul className='nav'>
-                        <CustomLink className='home' to="/">Home</CustomLink>
+                        <CustomLink  to="/">Home</CustomLink>
                         <CustomLink to="/About">About</CustomLink>
                         <CustomLink to="/Contact">Contact</CustomLink>
                         <CustomLink to="/Portfolios">Portfolios</CustomLink>
